d2c68b5efb1c08f3f065c6a5efa9db8fceec3573
beatricekallen <bkallen90@gmail.com>
2022-05-07 11:18:25 -0400

    updates file names and structure

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
-const { prompt } = require("inquirer");
-const {
+const db = require("./connection");
+
+async function findAllDepartments() {
+  return db.query("SELECT * FROM department;");
+}
+
+async function findAllRoles() {
+  return db.query(
+    "SELECT role.id, role.title, role.salary, department.name, role.department_id FROM role LEFT JOIN department ON role.department_id = department.id;"
+  );
+}
+
+async function findAllEmployees() {
+  return db.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id;"
+  );
+}
+
+async function createDepartment(name) {
+  return db.query("INSERT INTO DEPARTMENT (name) VALUES (?);", name);
+}
+
+async function createRole(title, salary, department_id) {
+  return db.query(
+    "INSERT INTO ROLE (title, salary, department_id) VALUES (?, ?, ?);",
+    [title, salary, department_id]
+  );
+}
+
+async function createEmployee(firstName, lastName, role, manager) {
+  return db.query(
+    "INSERT INTO EMPLOYEE (first_name, last_name) VALUES (?, ?);",
+    [firstName, lastName]
+  );
+}
+
+async function editEmployeeRole(roleId, employeeId) {
+  return db.query("UPDATE EMPLOYEE SET role_id = ? WHERE id = ?;", [
+    roleId,
+    employeeId,
+  ]);
+}
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
+module.exports = {
   findAllDepartments,
   findAllRoles,
   findAllEmployees,
@@ -7,226 +57,4 @@ const {
   createRole,
   createEmployee,
   editEmployeeRole,
-} = require("./app");
-const cTable = require("console.table");
-
-// reprint menu after each operation, use console.table to display tables, ability to quit out of app needs to be in menu
-async function mainMenu() {
-  const response = await prompt({
-    type: "list",
-    name: "choice",
-    message: "What would you like to do?",
-    choices: [
-      {
-        name: "View all departments",
-        value: "VIEW_DEPARTMENTS",
-      },
-      {
-        name: "View all roles",
-        value: "VIEW_ROLES",
-      },
-      {
-        name: "View all employees",
-        value: "VIEW_EMPLOYEES",
-      },
-      {
-        name: "Add a department",
-        value: "ADD_A_DEPARTMENT",
-      },
-      {
-        name: "Add a role",
-        value: "ADD_A_ROLE",
-      },
-      {
-        name: "Add an employee",
-        value: "ADD_AN_EMPLOYEE",
-      },
-      {
-        name: "Update an employee role",
-        value: "UPDATE_AN_EMPLOYEE_ROLE",
-      },
-      {
-        name: "Exit",
-        value: "EXIT",
-      },
-    ],
-  });
-
-  const choice = response.choice;
-
-  switch (choice) {
-    case "VIEW_DEPARTMENTS":
-      return viewAllDepartments();
-    case "VIEW_ROLES":
-      return viewAllRoles();
-    case "VIEW_EMPLOYEES":
-      return viewAllEmployees();
-    case "ADD_A_DEPARTMENT":
-      return addDepartment();
-    case "ADD_A_ROLE":
-      return addRole();
-    case "ADD_AN_EMPLOYEE":
-      return addEmployee();
-    case "UPDATE_AN_EMPLOYEE_ROLE":
-      return updateEmployeeRole();
-    case "EXIT":
-      return quit();
-  }
-}
-
-async function viewAllDepartments() {
-  const [rows] = await findAllDepartments();
-  const departments = rows;
-
-  console.table(departments);
-
-  mainMenu();
-}
-
-async function viewAllRoles() {
-  const [rows] = await findAllRoles();
-  const roles = rows;
-
-  console.table(rows);
-  mainMenu();
-}
-
-async function viewAllEmployees() {
-  const [rows] = await findAllEmployees();
-  const employees = rows;
-
-  console.table(employees);
-
-  mainMenu();
-}
-
-async function addDepartment() {
-  const response = await prompt([
-    {
-      type: "input",
-      name: "department",
-      message: "What is the name of the department you'd like to add?",
-    },
-  ]);
-
-  const createDepartmentRows = await createDepartment(response.department);
-
-  console.log("Department added.\n");
-
-  viewAllDepartments();
-}
-
-async function addRole() {
-  const [departmentRows] = await findAllDepartments();
-
-  const choices = departmentRows.map((department) => ({
-    name: department.name,
-    value: department.id,
-  }));
-
-  const response = await prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is the name of the role you'd like to add?",
-    },
-    {
-      type: "input",
-      name: "salary",
-      message: "What is the salary for the role you'd like to add?",
-    },
-    {
-      type: "list",
-      name: "choice",
-      message: "What is the name of the department you'd like to add?",
-      choices,
-    },
-  ]);
-  const salaryInt = parseInt(response.salary);
-  console.log(response.title, salaryInt, response.choice);
-
-  const [createRoleRows] = await createRole(
-    response.title,
-    salaryInt,
-    response.choice
-  );
-  console.log("Role added.\n");
-
-  viewAllRoles();
-}
-
-async function addEmployee() {
-  const response = await prompt([
-    {
-      type: "choice",
-      name: "firstName",
-      message: "What is the first name of the employee you'd like to add?",
-    },
-    {
-      type: "input",
-      name: "lastName",
-      message: "What is the last name of the employee you'd like to add?",
-    },
-    {
-      type: "input",
-      name: "jobTitle",
-      message: "What is the job title of the employee you'd like to add?",
-    },
-    {
-      type: "input",
-      name: "manager",
-      message: "Who is the manager of the employee you'd like to add?",
-    },
-  ]);
-
-  createEmployee(
-    response.firstName,
-    response.lastName,
-    response.jobTitle,
-    response.manager
-  );
-  console.log("Employee added.\n");
-  viewAllEmployees();
-}
-
-async function updateEmployeeRole() {
-  const [employeeRows] = await findAllEmployees();
-
-  const employeeChoices = employeeRows.map((employee) => ({
-    name: `${employee.first_name} ${employee.last_name}`,
-    value: employee.id,
-  }));
-
-  const [roleRows] = await findAllRoles();
-
-  const roleChoices = roleRows.map((role) => ({
-    name: role.title,
-    value: role.id,
-  }));
-
-  const { employeeChoice, roleChoice } = await prompt([
-    {
-      type: "list",
-      name: "employeeChoice",
-      message:
-        "What is the name of the employee whose role you'd like to update?",
-      choices: employeeChoices,
-    },
-    {
-      type: "list",
-      name: "roleChoice",
-      message: "What is the new role for this employee?",
-      choices: roleChoices,
-    },
-  ]);
-  editEmployeeRole(roleChoice, employeeChoice);
-  console.log("Employee role updated.\n");
-  viewAllEmployees();
-}
-
-function quit() {
-  console.log("Goodbye!");
-  process.exit();
-}
-
-mainMenu();
+};
